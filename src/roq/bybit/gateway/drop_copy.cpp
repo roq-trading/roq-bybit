9f8fb4ee7f17ec6ac19e46e5af431ff9dbca61e7
thraneh <thraneh@gmail.com>
2026-08-25 10:52:31 +0000

    chore

--- src/roq/bybit/gateway/drop_copy.cpp
+++ src/roq/bybit/gateway/drop_copy.cpp
@@ -300,11 +300,11 @@ void DropCopy::operator()(Trace<protocol::json::Auth> const &event) {
       subscribe();
     } else {
       if (shared_.settings.experimental.retry_logon) {
-        log::error("auth={}"sv, auth);
+        log::error("[{}] auth={}"sv, account_.name, auth);
         log::warn("Disconnecting..."sv);
         (*connection_).close();
       } else {
-        log::fatal("auth={}"sv, auth);
+        log::fatal("[{}] auth={}"sv, account_.name, auth);
       }
     }
   });
